9f1dc978a31b2e7fa1d4c7c2ac8baa0256c7cc1c
Dmitry Klionsky <dm.klionsky@gmail.com>
2018-12-04 18:02:07 +0300

    Port to new layout

--- _includes/sources/IsPalindromeString/Main.java
+++ _includes/sources/IsPalindromeString/Main.java
@@ -1,19 +1,21 @@
 class Main {
     public static boolean isPalindrome(String s) {
+        //SOLUTION_BEGIN
         int n = s.length();
         for (int i = 0; i < n/2; i++) {
             if (s.charAt(i) != s.charAt(n-i-1))
                 return false;
         }
         return true;
+        //SOLUTION_END
     }
 
     public static void main(String[] args) {
         try {
-            assert isPalindrome("");
-            assert isPalindrome("a");
-            assert isPalindrome("abcba");
-            assert !isPalindrome("abcde");
+            Assert.assertTrue(isPalindrome(""));
+            Assert.assertTrue(isPalindrome("a"));
+            Assert.assertTrue(isPalindrome("abcba"));
+            Assert.assertFalse(isPalindrome("abcde"));
             System.out.println("OK");
         } catch (Throwable e) {
             System.out.println("FAIL");
--- _includes/sources/lib/Assert.java
+++ _includes/sources/lib/Assert.java
@@ -1,6 +1,20 @@
 import java.util.Arrays;
 
 public class Assert {
+    public static void assertTrue(boolean condition) throws Throwable {
+        if (!condition) {
+            System.out.println("Expected true");
+            throw new Throwable();
+        }
+    }
+
+    public static void assertFalse(boolean condition) throws Throwable {
+        if (condition) {
+            System.out.println("Expected false");
+            throw new Throwable();
+        }
+    }
+
     public static void assertEquals(int exp, int act) throws Throwable {
         if (exp != act) {
             System.out.println("Expected: " + exp + ", but saw: " + act);
