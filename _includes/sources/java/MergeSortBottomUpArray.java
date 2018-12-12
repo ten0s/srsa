4701ed31073f10a7523d328ec5b61b05f72e1db1
Dmitry Klionsky <dm.klionsky@gmail.com>
2018-12-12 16:06:20 +0300

    Refine

--- _includes/sources/java/MergeSortBottomUpArray.java
+++ _includes/sources/java/MergeSortBottomUpArray.java
@@ -20,8 +20,8 @@ class MergeSortBottomUpArray {
         for (int k = lo; k <= hi; k++) {
             if      (i > mid)              a[k] = aux[j++];
             else if (j > hi)               a[k] = aux[i++];
-            else if (less(aux[i], aux[j])) a[k] = aux[i++];
-            else                           a[k] = aux[j++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
         }
     }
     // SOLUTION_END
