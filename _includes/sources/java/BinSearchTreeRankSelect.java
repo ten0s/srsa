dd5d4482d56fcc012a18de6af8c2e820d6db3705
Dmitry Klionsky <dm.klionsky@gmail.com>
2018-12-21 19:16:09 +0300

    Add task

---
+++ _includes/sources/java/BinSearchTreeRankSelect.java
@@ -0,0 +1,53 @@
+import java.util.NoSuchElementException;
+
+/*
+public class BinTree.Node<T> {
+    public T item;
+    public Node<T> left;
+    public Node<T> right;
+    public int size(Node<T> node);
+}
+*/
+
+public class BinSearchTreeRankSelect {
+    public static <K extends Comparable<K>> int rank(K key, BinTree.Node<K> n) {
+        // SOLUTION_BEGIN
+        if (n == null) return 0;
+        int cmp = key.compareTo(n.item);
+        if      (cmp < 0) return rank(key, n.left);
+        else if (cmp > 0) return 1 + BinTree.size(n.left) + rank(key, n.right);
+        else              return BinTree.size(n.left);
+        // SOLUTION_END
+    }
+
+    public static <K extends Comparable<K>> BinTree.Node<K> select(int k, BinTree.Node<K> n) {
+        // SOLUTION_BEGIN
+        if (n == null) return null;
+        int t = BinTree.size(n.left);
+        if      (t > k) return select(k, n.left);
+        else if (t < k) return select(k-t-1, n.right);
+        else            return n;
+        // SOLUTION_END
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws Throwable {
+        int[] keys = new int[] {1,3,5,6,7,9};
+        BinTree.Node<Integer> tree = BinTree.fromIntArray(keys);
+        //BinTree.println(tree);
+
+        // Property #1
+        // for i in [0..size(keys)-1] { i == rank(select(i)) }
+        for (int i = 0; i < keys.length; i++) {
+            Assert.assertEquals(i, rank(select(i, tree).item, tree));
+        }
+
+        // Property #2
+        // for all key in keys { key == select(rank(key)) }
+        for (int key : keys) {
+            Assert.assertEquals(key, select(rank(key, tree), tree).item);
+        }
+
+        System.out.println("OK");
+    }
+}
---
+++ exercises/BinSearchTreeRankSelectJava.md
@@ -0,0 +1,19 @@
+---
+layout: exercise
+title: Binary Search Tree Rank/Selection
+question_text: >-
+  Find <i>rank</i> (the number of keys less than a given key) and
+  <i>selection</i> (the key with a given rank) in
+  the binary search tree
+solution_file: /sources/java/BinSearchTreeRankSelect.java
+library_files:
+- /sources/java/BinTree.java
+- /sources/java/Entry.java
+- /sources/java/ListQueue.java
+- /sources/java/Node.java
+- /sources/java/Pair.java
+- /sources/java/Assert.java
+- /sources/java/ArrayUtil.java
+language: java
+command: javac BinSearchTreeRankSelect.java && java BinSearchTreeRankSelect
+---
