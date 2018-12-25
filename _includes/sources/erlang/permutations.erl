5f10d3fa5c6e302ec0f08a66b0c0cd3bbf1ea9db
Dmitry Klionsky <dm.klionsky@gmail.com>
2018-12-25 11:32:53 +0300

    Add task

---
+++ _includes/sources/erlang/permutations.erl
@@ -0,0 +1,22 @@
+-module(permutations).
+-export([main/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+-spec perms([T]) -> [[T]].
+%% SOLUTION_BEGIN
+perms([]) ->
+    [[]];
+perms(L) ->
+    [[H | T] || H <- L, T <- perms(L -- [H])].
+%% SOLUTION_END
+
+main(_) ->
+    case eunit:test(?MODULE) of
+    ok -> halt(0);
+    _  -> halt(1)
+    end.
+
+perms_test() ->
+    ?assertEqual([], perms([])),
+    ?assertEqual([[b,u,g],[b,g,u],[u,b,g],[u,g,b],[g,b,u],[g,u,b]],
+                 perms([b,u,g])).
---
+++ exercises/PermutationsErlang.md
@@ -0,0 +1,9 @@
+---
+layout: exercise
+title: Permutations
+question_text: Generate all permutations of the elements in a list
+solution_file: /sources/erlang/permutations.erl
+library_files:
+language: erlang
+command: erlc permutations.erl && erl -noshell -eval "permutations:main({})."
+---
