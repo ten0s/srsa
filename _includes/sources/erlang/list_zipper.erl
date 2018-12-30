6cc94039197aea9f019cfe6ba72cda2710577121
Dmitry Klionsky <dm.klionsky@gmail.com>
2018-12-30 17:21:33 +0300

    Add task

---
+++ _includes/sources/erlang/list_zipper.erl
@@ -0,0 +1,111 @@
+-module(list_zipper).
+-export([main/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+-type zlist(T) :: {[T], [T]}. % {Prev, [Curr | Next]}
+-type error(_E) :: no_return().
+
+-spec new() -> zlist(_T).
+%% SOLUTION_BEGIN
+new() ->
+    {[], []}.
+%% SOLUTION_END
+
+-spec is_empty(zlist(_T)) -> boolean().
+%% SOLUTION_BEGIN
+is_empty({[], []}) ->
+    true;
+is_empty(_) ->
+    false.
+%% SOLUTION_END
+
+-spec from_list([T]) -> zlist(T).
+%% SOLUTION_BEGIN
+from_list(L) ->
+    {[], L}.
+%% SOLUTION_END
+
+-spec to_list(zlist(T)) -> [T].
+%% SOLUTION_BEGIN
+to_list({Prev, Next}) ->
+    lists:reverse(Prev) ++ Next.
+%% SOLUTION_END
+
+-spec prev(zlist(T)) -> zlist(T) | error(empty).
+%% SOLUTION_BEGIN
+prev({[], _}) ->
+    error(empty);
+prev({[P | Prev], Next}) ->
+    {Prev, [P | Next]}.
+%% SOLUTION_END
+
+-spec next(zlist(T)) -> zlist(T) | error(empty).
+%% SOLUTION_BEGIN
+next({_, []}) ->
+    error(empty);
+next({Prev, [C | Next]}) ->
+    {[C | Prev], Next}.
+%% SOLUTION_END
+
+-spec current(zlist(T)) -> T | error(empty).
+%% SOLUTION_BEGIN
+current({_, []}) ->
+    error(empty);
+current({_, [C | _]}) ->
+    C.
+%% SOLUTION_END
+
+-spec update(T, zlist(T)) -> zlist(T) | error(empty).
+%% SOLUTION_BEGIN
+update(_V, {_, []}) ->
+    error(empty);
+update(V, {Prev, [_ | Next]}) ->
+    {Prev, [V | Next]}.
+%% SOLUTION_END
+
+-spec insert(T, zlist(T)) -> zlist(T).
+%% SOLUTION_BEGIN
+insert(V, {Prev, Next}) ->
+    {Prev, [V | Next]}.
+%% SOLUTION_END
+
+-spec delete(zlist(T)) -> zlist(T) | error(empty).
+%% SOLUTION_BEGIN
+delete({_, []}) ->
+    error(empty);
+delete({Prev, [_ | Next]}) ->
+    {Prev, Next}.
+%% SOLUTION_END
+
+main(_) ->
+    case eunit:test(?MODULE) of
+    ok -> halt(0);
+    _  -> halt(1)
+    end.
+
+zlist_test() ->
+    ?assert(is_empty(new())),
+    L = [4,8,10,9,1],
+    ?assertEqual([], to_list(new())),
+    ?assertEqual(L, to_list(from_list(L))),
+    Z0 = from_list(L),
+    ?assertNot(is_empty(Z0)),
+    ?assertEqual(4, current(Z0)),
+    ?assertError(empty, prev(Z0)),
+    Z1 = next(Z0),
+    ?assertEqual(8, current(Z1)),
+    Z2 = update(3, Z1),
+    ?assertEqual(3, current(Z2)),
+    Z3 = prev(Z2),
+    ?assertEqual(4, current(Z3)),
+    Z4 = next(next(next(Z3))),
+    Z5 = insert(2, Z4),
+    ?assertEqual(2, current(Z5)),
+    Z6 = prev(Z5),
+    ?assertEqual(10, current(Z6)),
+    Z7 = delete(Z6),
+    ?assertEqual(2, current(Z7)),
+    Z8 = next(next(next(Z7))),
+    ?assertError(empty, current(Z8)),
+    ?assertError(empty, next(Z8)),
+    ?assertError(empty, delete(Z8)).
---
+++ exercises/ListZipperErlang.md
@@ -0,0 +1,9 @@
+---
+layout: exercise
+title: List Zipper
+question_text: Implement list zipper
+solution_file: /sources/erlang/list_zipper.erl
+library_files:
+language: erlang
+command: erlc list_zipper.erl && erl -noshell -eval "list_zipper:main({})."
+---
