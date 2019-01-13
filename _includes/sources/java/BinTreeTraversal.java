e47765efa4fbe0e111ec4c06fde74bab748d6560
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-01-13 12:02:57 +0300

    Review

--- _includes/sources/java/BinTreeTraversal.java
+++ _includes/sources/java/BinTreeTraversal.java
@@ -11,40 +11,31 @@ public class BinTree.Node<T> {
 public class BinTreeTraversal {
     public static <T, R> R preOrder(BiFunction<T, R, R> fun, R init, BinTree.Node<T> tree) {
         // SOLUTION_BEGIN
-        if (tree == null) {
-            return init;
-        } else {
-            R n = fun.apply(tree.item, init);
-            R l = inOrder(fun, n, tree.left);
-            R r = inOrder(fun, l, tree.right);
-            return r;
-        }
+        if (tree == null) return init;
+        R n = fun.apply(tree.item, init);
+        R l = inOrder(fun, n, tree.left);
+        R r = inOrder(fun, l, tree.right);
+        return r;
         // SOLUTION_END
     }
 
     public static <T, R> R inOrder(BiFunction<T, R, R> fun, R init, BinTree.Node<T> tree) {
         // SOLUTION_BEGIN
-        if (tree == null) {
-            return init;
-        } else {
-            R l = inOrder(fun, init, tree.left);
-            R n = fun.apply(tree.item, l);
-            R r = inOrder(fun, n, tree.right);
-            return r;
-        }
+        if (tree == null) return init;
+        R l = inOrder(fun, init, tree.left);
+        R n = fun.apply(tree.item, l);
+        R r = inOrder(fun, n, tree.right);
+        return r;
         // SOLUTION_END
     }
 
     public static <T, R> R postOrder(BiFunction<T, R, R> fun, R init, BinTree.Node<T> tree) {
         // SOLUTION_BEGIN
-        if (tree == null) {
-            return init;
-        } else {
-            R l = inOrder(fun, init, tree.left);
-            R r = inOrder(fun, l, tree.right);
-            R n = fun.apply(tree.item, r);
-            return n;
-        }
+        if (tree == null) return init;
+        R l = inOrder(fun, init, tree.left);
+        R r = inOrder(fun, l, tree.right);
+        R n = fun.apply(tree.item, r);
+        return n;
         // SOLUTION_END
     }
 
