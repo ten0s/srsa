e00f3824df8eb7619f90c0ae387c36fdc347de44
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-01-16 15:22:58 +0300

    Add task

---
+++ _includes/sources/java/GraphBipartite.java
@@ -0,0 +1,84 @@
+import java.util.Iterator;
+import java.util.Collections;
+
+public class GraphBipartite {
+    // SOLUTION_BEGIN
+    private boolean[] marked;
+    private boolean[] color;
+    private boolean isBipartite = true;
+    // SOLUTION_END
+
+    public GraphBipartite(Graph G) {
+        // SOLUTION_BEGIN
+        marked = new boolean[G.V()];
+        color = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(v, G);
+            }
+        }
+        // SOLUTION_END
+    }
+
+    public boolean isBipartite() {
+        // SOLUTION_BEGIN
+        return isBipartite;
+        // SOLUTION_END
+    }
+
+    private void dfs(int v, Graph G) {
+        // SOLUTION_BEGIN
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                color[w] = !color[v];
+                dfs(w, G);
+            } else {
+                if (color[w] == color[v]) {
+                    isBipartite = false;
+                }
+            }
+        }
+        // SOLUTION_END
+    }
+
+    public static void main(String[] args) throws Throwable {
+        // /sources/data/graph1.txt
+        Graph G = new Graph(10);
+        G.addEdge(0, 1);
+        G.addEdge(0, 7);
+        G.addEdge(1, 2);
+        G.addEdge(1, 3);
+        G.addEdge(1, 7);
+        G.addEdge(1, 8);
+        G.addEdge(2, 3);
+        G.addEdge(3, 4);
+        G.addEdge(3, 5);
+        G.addEdge(3, 8);
+        G.addEdge(4, 5);
+        G.addEdge(5, 6);
+        G.addEdge(5, 7);
+        G.addEdge(5, 8);
+        G.addEdge(6, 7);
+        G.addEdge(7, 8);
+        GraphBipartite b = new GraphBipartite(G);
+        Assert.assertFalse(b.isBipartite());
+
+        // /sources/data/graph4.txt
+        // /sources/data/graph-bipartite.txt
+        Graph G4 = new Graph(7);
+        G4.addEdge(0, 1);
+        G4.addEdge(0, 2);
+        G4.addEdge(0, 5);
+        G4.addEdge(0, 6);
+        G4.addEdge(1, 3);
+        G4.addEdge(2, 3);
+        G4.addEdge(2, 4);
+        G4.addEdge(4, 5);
+        G4.addEdge(4, 6);
+        GraphBipartite b4 = new GraphBipartite(G4);
+        Assert.assertTrue(b4.isBipartite());
+
+        System.out.println("OK");
+    }
+}
---
+++ exercises/java/GraphBipartite.md
@@ -0,0 +1,23 @@
+---
+layout: exercise
+title: Graph Bipartite
+question_text: >-
+  Is a given graph bipartite?
+  <table border="1">
+    <tr>
+      <td><img relative_src="/assets/images/graph1.png" title="graph1"></td>
+      <td><img relative_src="/assets/images/graph-bipartite.png" title="graph-bipartite"></td>
+    </tr>
+  </table>
+solution_file: /sources/java/GraphBipartite.java
+library_files:
+- /sources/java/Assert.java
+- /sources/java/ArrayUtil.java
+- /sources/java/Graph.java
+- /sources/java/Bag.java
+- /sources/java/HashSet.java
+- /sources/java/Queue.java
+- /sources/java/Prime.java
+language: java
+command: javac GraphBipartite.java && java GraphBipartite
+---
