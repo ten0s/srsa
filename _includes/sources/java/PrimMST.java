062f2870c0f8862b20190a64e5a278306bb21d07
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-01-29 11:22:59 +0300

    Add task

---
+++ _includes/sources/java/PrimMST.java
@@ -0,0 +1,108 @@
+/*
+public class public class Queue<Item> implements Iterable<Item> {
+    public Queue();
+    public void enqueue(Item item);
+    public int size();
+}
+
+class MinPQ<Key extends Comparable<Key>> {
+    public MinPQ();
+    public void insert(Key v);
+    public Key delMin();
+    public boolean isEmpty();
+}
+*/
+
+public class PrimMST {
+    // SOLUTION_BEGIN
+    private boolean[] marked;
+    private MinPQ<Edge> pq;
+    private Queue<Edge> mst;
+    private double weight;
+    // SOLUTION_END
+
+    public PrimMST(EdgeWeightedGraph G) {
+        // SOLUTION_BEGIN
+        marked = new boolean[G.V()];
+        pq = new MinPQ<>();
+        mst = new Queue<>();
+
+        visit(G, 0);
+        while (!pq.isEmpty() && mst.size() < G.V() - 1) {
+            Edge e = pq.delMin();
+            int v = e.either(), w = e.other(v);
+            if (marked[v] && marked[w]) continue;
+            mst.enqueue(e);
+            weight += e.weight();
+            if (!marked[v]) visit(G, v);
+            if (!marked[w]) visit(G, w);
+        }
+        // SOLUTION_END
+    }
+
+    // SOLUTION_BEGIN
+    private void visit(EdgeWeightedGraph G, int v) {
+        marked[v] = true;
+        for (Edge e : G.adj(v)) {
+            if (!marked[e.other(v)]) {
+                pq.insert(e);
+            }
+        }
+    }
+    // SOLUTION_END
+
+    public Iterable<Edge> edges() {
+        // SOLUTION_BEGIN
+        return mst;
+        // SOLUTION_END
+    }
+
+    public double weight() {
+        // SOLUTION_BEGIN
+        return weight;
+        // SOLUTION_END
+    }
+
+    public static void main(String[] args) throws Throwable {
+        // /data/tinyEWG.txt
+        EdgeWeightedGraph G = new EdgeWeightedGraph(8);
+        G.addEdge(new Edge(4, 5, 0.35));
+        G.addEdge(new Edge(4, 7, 0.37));
+        G.addEdge(new Edge(5, 7, 0.28));
+        G.addEdge(new Edge(0, 7, 0.16));
+        G.addEdge(new Edge(1, 5, 0.32));
+        G.addEdge(new Edge(0, 4, 0.38));
+        G.addEdge(new Edge(2, 3, 0.17));
+        G.addEdge(new Edge(1, 7, 0.19));
+        G.addEdge(new Edge(0, 2, 0.26));
+        G.addEdge(new Edge(1, 2, 0.36));
+        G.addEdge(new Edge(1, 3, 0.29));
+        G.addEdge(new Edge(2, 7, 0.34));
+        G.addEdge(new Edge(6, 2, 0.40));
+        G.addEdge(new Edge(3, 6, 0.52));
+        G.addEdge(new Edge(6, 0, 0.58));
+        G.addEdge(new Edge(6, 4, 0.93));
+        //System.out.println(G);
+        //System.out.println(G.toDot());
+
+        HashSet<Edge> set = new HashSet<>();
+        set.add(new Edge(0, 7, 0.16));
+        set.add(new Edge(2, 3, 0.17));
+        set.add(new Edge(1, 7, 0.19));
+        set.add(new Edge(0, 2, 0.26));
+        set.add(new Edge(5, 7, 0.28));
+        set.add(new Edge(4, 5, 0.35));
+        set.add(new Edge(6, 2, 0.40));
+
+        PrimMST mst = new PrimMST(G);
+        for (Edge e : mst.edges()) {
+            Assert.assertTrue(set.contains(e));
+            set.delete(e);
+        }
+        Assert.assertTrue(set.isEmpty());
+
+        Assert.assertEquals(1.81, mst.weight());
+
+        System.out.println("OK");
+    }
+}
---
+++ exercises/java/PrimMST.md
@@ -0,0 +1,25 @@
+---
+layout: exercise
+title: Prim's MST
+question_text: >-
+  Implement Prim's Minimum Spanning Tree
+  <table border="1">
+    <tr>
+      <td><img relative_src="/assets/images/tinyEWG.png" title="tinyEWG"></td>
+    </tr>
+  </table>
+solution_file: /sources/java/PrimMST.java
+library_files:
+- /sources/java/Assert.java
+- /sources/java/ArrayUtil.java
+- /sources/java/Bag.java
+- /sources/java/Edge.java
+- /sources/java/EdgeWeightedGraph.java
+- /sources/java/PQ.java
+- /sources/java/MinPQ.java
+- /sources/java/HashSet.java
+- /sources/java/Queue.java
+- /sources/java/Prime.java
+language: java
+command: javac PrimMST.java && java PrimMST
+---
