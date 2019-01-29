3bb0821a4907a7f1b0a82aad603bdc96aa6133ff
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-01-29 16:33:37 +0300

    Review

--- _includes/sources/java/IsPalindromeNumber.java
+++ _includes/sources/java/IsPalindromeNumber.java
@@ -1,7 +1,7 @@
 class IsPalindromeNumber {
     public static boolean isPalindrome(int n) {
         // SOLUTION_BEGIN
-        if (n < 0) return isPalindrome(-n);
+        if (n < 0) return false;
         if (n < 10) return true;
         int base = (int) Math.pow(10, (int) Math.log10(n));
         int big = n / base;
@@ -15,11 +15,12 @@ class IsPalindromeNumber {
     }
 
     public static void main(String[] args) throws Throwable {
-        Assert.assertTrue(isPalindrome(-3));
+        Assert.assertFalse(isPalindrome(-3));
         Assert.assertTrue(isPalindrome(3));
         Assert.assertTrue(isPalindrome(232));
         Assert.assertTrue(isPalindrome(24542));
         Assert.assertFalse(isPalindrome(123));
+        Assert.assertFalse(isPalindrome(1000021));
         System.out.println("OK");
     }
 }
--- exercises/java/IsPalindromeNumber.md
+++ exercises/java/IsPalindromeNumber.md
@@ -1,7 +1,9 @@
 ---
 layout: exercise
 title: Is Number a Palindrome
-question_text: Is the number a palindrome?
+question_text: >-
+  Is the number a palindrome?
+  <a href="https://leetcode.com/problems/palindrome-number">More Practise</a>
 solution_file: /sources/java/IsPalindromeNumber.java
 library_files:
 - /sources/java/Assert.java
