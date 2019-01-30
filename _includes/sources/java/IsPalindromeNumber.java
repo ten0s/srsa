53f31d3d0cd0909106e7a5cb90992cc58690798c
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-01-30 11:50:15 +0300

    Review

--- _includes/sources/java/IsPalindromeNumber.java
+++ _includes/sources/java/IsPalindromeNumber.java
@@ -3,20 +3,24 @@ class IsPalindromeNumber {
         // SOLUTION_BEGIN
         if (n < 0) return false;
         if (n < 10) return true;
-        int base = (int) Math.pow(10, (int) Math.log10(n));
-        int big = n / base;
-        int little = n % 10;
-        if (big == little) {
-            int m = (n - big * base - little) / 10;
-            return isPalindrome(m);
+        int p = (int) Math.log10(n);
+        for (int i = 0; i <= p/2; i++) {
+            if (digit(i, n) != digit(p-i, n)) return false;
         }
-        return false;
+        return true;
         // SOLUTION_END
     }
+
+    // SOLUTION_BEGIN
+    private static int digit(int i, int n) {
+        return (int) (n / Math.pow(10, i)) % 10;
+    }
+    // SOLUTION_END
 
     public static void main(String[] args) throws Throwable {
         Assert.assertFalse(isPalindrome(-3));
         Assert.assertTrue(isPalindrome(3));
+        Assert.assertFalse(isPalindrome(10));
         Assert.assertTrue(isPalindrome(232));
         Assert.assertTrue(isPalindrome(24542));
         Assert.assertFalse(isPalindrome(123));
