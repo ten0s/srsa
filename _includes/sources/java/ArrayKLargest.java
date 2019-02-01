403f9f992ca8dbb70ef577117e25bafa8bced1dc
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-02-01 16:35:24 +0300

    Add task

---
+++ _includes/sources/java/ArrayKLargest.java
@@ -0,0 +1,27 @@
+public class ArrayKLargest {
+    public static int[] largest(int k, int[] a) {
+        // SOLUTION_BEGIN
+        MinPQ<Integer> pq = new MinPQ<>();
+        for (int i = 0; i < a.length; i++) {
+            pq.insert(a[i]);
+            if (pq.size() > k) {
+                pq.delMin();
+            }
+        }
+        int[] r = new int[pq.size()];
+        int i = r.length-1;
+        while (!pq.isEmpty()) {
+            r[i--] = pq.delMin();
+        }
+        return r;
+        // SOLUTION_END
+    }
+
+    public static void main(String[] args) throws Throwable {
+        int[] a = new int[] {1,23,12,9,30,2,50};
+        Assert.assertArrayEquals(new int[] {50}, largest(1, a));
+        Assert.assertArrayEquals(new int[] {50,30,23}, largest(3, a));
+        Assert.assertArrayEquals(new int[] {50,30,23,12,9,2,1}, largest(7, a));
+        System.out.println("OK");
+    }
+}
---
+++ exercises/java/ArrayKLargest.md
@@ -0,0 +1,13 @@
+---
+layout: exercise
+title: Array K Largest
+question_text: Find K largest items from an unordered array of integers
+solution_file: /sources/java/ArrayKLargest.java
+library_files:
+- /sources/java/Assert.java
+- /sources/java/ArrayUtil.java
+- /sources/java/MinPQ.java
+- /sources/java/PQ.java
+language: java
+command: javac ArrayKLargest.java && java ArrayKLargest
+---
