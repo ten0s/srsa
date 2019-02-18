417cb3e2736eeabfe2760a5def6ef48393f809d9
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-02-18 18:37:03 +0300

    Add task

---
+++ _includes/sources/java/KeyIndexedCounting.java
@@ -0,0 +1,33 @@
+public class KeyIndexedCounting {
+    public static int[] sort(int[] a, int R) {
+        //+BEGIN_SOLUTION
+        int N = a.length;
+        int[] aux = new int[N];
+
+        int[] count = new int[R+1];
+
+        for (int i = 0; i < N; i++)
+            count[a[i]+1]++;
+
+        for (int r = 0; r < R; r++)
+            count[r+1] += count[r];
+
+        for (int i = 0; i < N; i++)
+            aux[count[a[i]]++] = a[i];
+
+        for (int i = 0; i < N; i++)
+            a[i] = aux[i];
+
+        return a;
+        //+END_SOLUTION
+    }
+
+    public static void main(String[] args) throws Throwable {
+        int R = 6;
+        int[] a = new int[] {3,1,4,0,5,2,5,4,3,2,1,0,0,1,2,3,4,5};
+        int[] exp = new int[] {0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5};
+        sort(a, R);
+        Assert.assertArrayEquals(exp, sort(a, R));
+        System.out.println("OK");
+    }
+}
---
+++ exercises/java/KeyIndexedCounting.md
@@ -0,0 +1,11 @@
+---
+layout: exercise
+title: Key-Indexed Counting
+question_text: Sort an array integers between 0 and R-1
+solution_file: /sources/java/KeyIndexedCounting.java
+library_files:
+- /sources/java/Assert.java
+- /sources/java/ArrayUtil.java
+language: java
+command: javac KeyIndexedCounting.java && java KeyIndexedCounting
+---
