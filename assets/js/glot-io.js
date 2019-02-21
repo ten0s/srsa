9f7cbd1ea54abe294376ae5304132e2235778293
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-02-21 18:26:54 +0300

    Refactor

---
+++ assets/js/glot-io.js
@@ -0,0 +1,33 @@
+/*jslint
+   browser: true,
+   devel: true,
+   nomen: true
+*/
+/*global $ */
+
+"use strict";
+
+var GlotIO = (function () {
+    function run(req) {
+        // run.glot.io doesn't support cors
+        // workaround through https://cors-anywhere.herokuapp.com/
+        var url = "https://cors-anywhere.herokuapp.com/" +
+            "https://run.glot.io/languages/" + req.language + "/" + req.version;
+        return $.ajax({
+            url: url,
+            type: "POST",
+            headers: {
+                "Authorization": "Token " + Storage.getGlotIOToken(),
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                files: req.files,
+                command: req.command
+            })
+        });
+    }
+
+    return {
+        run: run
+    };
+}());
