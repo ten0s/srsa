df51500f10c75906b9444e2bb47dc0b8c668fa1c
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-03-10 13:05:31 +0300

    Refactor

---
+++ _includes/sources/erlang/leftist_heap_anim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+erlc leftist_heap.erl
+escript leftist_heap_anim.erl
+
+for dotfile in  $(find . -name 'leftist_heap-*.dot' | sort); do
+    cmdfile=${dotfile%.dot}.cmd
+    pngfile=${dotfile%.dot}.png
+    bigfile=${pngfile%.png}-big.png
+    endfile=${pngfile%.png}-end.png
+    dot -Tpng $dotfile -o $pngfile
+    convert $pngfile -gravity center -background white -extent 400x450 $bigfile
+    convert $bigfile -fill blue -gravity northwest -pointsize 30 -annotate 0 @$cmdfile $endfile
+done
+
+convert -delay 120 -loop 0 "*-end.png" leftist_heap.gif
+rm *.cmd *.dot *.png
