5cb3d3ffb620c416dfa52585a99c57ba5b4c79e1
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-06-09 10:16:19 +0300

    Add proper tests to erlang queue

--- _includes/sources/erlang/my_queue.erl
+++ _includes/sources/erlang/my_queue.erl
@@ -1,8 +1,21 @@
 -module(my_queue).
--export([new/0, is_empty/1, enqueue/2, dequeue/1, peek/1, from_list/1, to_list/1]).
+-export([
+    new/0, is_empty/1,
+    from_list/1, to_list/1,
+    enqueue/2, dequeue/1, peek/1
+]).
 %%+BEGIN_FOLD Tests {
 -export([main/1]).
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
+-behaviour(proper_statem).
+-export([
+    initial_state/0,
+    command/1,
+    precondition/2,
+    postcondition/3,
+    next_state/3
+]).
 %%+END_FOLD }
 
 %% Invariant must be maintained that F is empty only when R is empty.
@@ -22,6 +35,18 @@ is_empty({[], _}) ->
     true;
 is_empty({_, _}) ->
     false.
+%%+END_SOLUTION
+
+-spec from_list(list(T)) -> queue(T).
+%%+BEGIN_SOLUTION
+from_list(L) ->
+    {L, []}.
+%%+END_SOLUTION
+
+-spec to_list(queue(T)) -> list(T).
+%%+BEGIN_SOLUTION
+to_list({F, R}) ->
+    F ++ lists:reverse(R).
 %%+END_SOLUTION
 
 %% O(1)
@@ -52,18 +77,6 @@ peek({[X | _], _}) ->
     X.
 %%+END_SOLUTION
 
--spec from_list(list(T)) -> queue(T).
-%%+BEGIN_SOLUTION
-from_list(L) ->
-    {L, []}.
-%%+END_SOLUTION
-
--spec to_list(queue(T)) -> list(T).
-%%+BEGIN_SOLUTION
-to_list({F, R}) ->
-    F ++ lists:reverse(R).
-%%+END_SOLUTION
-
 %%+BEGIN_FOLD Tests {
 main(_) ->
     case eunit:test(?MODULE) of
@@ -90,9 +103,70 @@ queue_test() ->
     ?assertError(empty, dequeue(Q8)),
 
     L = [1,2,3,4],
-    ?assertEqual(L, to_list(from_list(L))),
     Q9 = {L, []} = from_list(L),
     {1, Q10 = {[2,3,4], []}} = dequeue(Q9),
     Q11 = {[2,3,4], [5]} = enqueue(5, Q10),
     ?assertEqual([2,3,4,5], to_list(Q11)).
+
+proper_test_() ->
+    {timeout, 5,
+        ?_assertEqual([], proper:module(?MODULE, [{to_file, user}, nocolors]))}.
+
+prop_from_to_list() ->
+    ?FORALL(L, list(),
+        L == to_list(from_list(L))).
+
+prop_queue() ->
+    ?FORALL(Cmds, commands(?MODULE),
+    begin
+        {H, S, Res} = run_commands(?MODULE, Cmds),
+        ?WHENFAIL(
+            io:format("~nHistory:~n~p~nState:~n~p~nRes:~n~p~n~n",
+                [H, S, Res]),
+            aggregate(command_names(Cmds), Res =:= ok))
+    end).
+
+-record(st, {
+    queue :: queue(integer()),
+    model :: list(integer())
+}).
+
+initial_state() ->
+    #st{queue = new(),
+        model = []}.
+
+command(#st{queue = Q}) ->
+    oneof([
+        {call, ?MODULE, is_empty, [Q]}
+      , {call, ?MODULE, enqueue, [integer(), Q]}
+      , {call, ?MODULE, dequeue, [Q]}
+      , {call, ?MODULE, peek, [Q]}
+    ]).
+
+precondition(S, {call, ?MODULE, dequeue, [_Q]}) ->
+    S#st.model =/= [];
+precondition(S, {call, ?MODULE, peek, [_Q]}) ->
+    S#st.model =/= [];
+precondition(_S, _Call) ->
+    true.
+
+next_state(S, _Ret, {call, ?MODULE, is_empty, [_Q0]}) ->
+    S;
+next_state(S, Q1, {call, ?MODULE, enqueue, [V, _Q0]}) ->
+    S#st{queue = Q1, model = S#st.model ++ [V]};
+next_state(S, T_Q1,  {call, ?MODULE, dequeue, [_Q0]}) ->
+    Q1 = {call,erlang,element,[2, T_Q1]},
+    S#st{queue = Q1, model = tl(S#st.model)};
+next_state(S, _Ret, _Call) ->
+    S.
+
+postcondition(S, {call, ?MODULE, is_empty, [_Q]}, Res) ->
+    Res == (S#st.model == []);
+postcondition(S, {call, ?MODULE, enqueue, [_V, _Q0]}, _Q1) ->
+    to_list(S#st.queue) == S#st.model;
+postcondition(S, {call, ?MODULE, dequeue, [_Q]}, {_V, _Q1}) ->
+    to_list(S#st.queue) == S#st.model;
+postcondition(S, {call, ?MODULE, peek, [_Q]}, Res) ->
+    Res == hd(S#st.model).
+
 %%+END_FOLD }
--- exercises/erlang/Queue.md
+++ exercises/erlang/Queue.md
@@ -4,6 +4,7 @@ title: Queue
 question_text: Implement queue
 solution_file: /sources/erlang/my_queue.erl
 library_files:
+- /sources/erlang/run.sh
+- /sources/erlang/lib/proper-1.3.tar.gz.base64
 language: erlang
-command: erlc my_queue.erl && erl -noshell -eval "my_queue:main([])."
----
+command: bash run.sh my_queue.erl
